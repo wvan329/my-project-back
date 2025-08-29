9af84991a88f17158a23b8a6dc9b1e6f68e8d5bd
wgk <2287334379@qq.com>
2025-08-29 20:45:35 +0800

    1

--- src/main/java/cn/ai/config/AiConfigure.java
+++ src/main/java/cn/ai/config/AiConfigure.java
@@ -2,6 +2,7 @@ package cn.ai.config;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.openai.OpenAiChatModel;
